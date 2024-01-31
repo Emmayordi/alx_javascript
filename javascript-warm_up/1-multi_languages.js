29ed27a0273100c39abb3ef5867c748876c0c902
Emmayordi <yordanosteklay424@gmail.com>
2024-01-31 21:16:27 +0100

    Write a script that prints 3 lines:

--- javascript-warm_up/1-multi_languages.js
+++ javascript-warm_up/1-multi_languages.js
@@ -1,18 +1,4 @@
-// Creating a "dictionary" using an object
-let myDictionary = {};
-
-// Adding key-value pairs
-myDictionary["key1"] = "value1";
-myDictionary["key2"] = "value2";
-myDictionary["key3"] = "value3";
-
-// Accessing values using keys
-console.log(myDictionary["key2"]); // Output: value2
-
-// Checking if a key exists in the dictionary
-console.log("key4" in myDictionary); // Output: false
-
-// Iterating through key-value pairs
-for (let key in myDictionary) {
-    console.log(key + ": " + myDictionary[key]);
-}
+// Use console.log(...) to print three lines of output
+console.log("C is fun");
+console.log("Python is cool");
+console.log("JavaScript is amazing");
