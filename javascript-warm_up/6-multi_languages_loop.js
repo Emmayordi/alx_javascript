b24f524b09fb0c6149a4d1e4ae79af1e88b433d9
Emmayordi <yordanosteklay424@gmail.com>
2024-01-31 19:26:41 +0100

    javescript

---
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,9 @@
+// Using let to declare variables
+let firstLine = "C is fun";
+let secondLine = "Python is cool";
+let thirdLine = "JavaScript is amazing";
+
+// Printing the lines using console.log
+console.log(firstLine);
+console.log(secondLine);
+console.log(thirdLine);
