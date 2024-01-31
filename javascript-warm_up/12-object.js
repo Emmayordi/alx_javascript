f421cb7ee56d70cec552da6bda30b8a525d897b3
Emmayordi <yordanosteklay424@gmail.com>
2024-01-31 21:35:31 +0100

    update that property's

--- javascript-warm_up/12-object.js
+++ javascript-warm_up/12-object.js
@@ -1,10 +1,11 @@
 // Original script with an object
 const myObject = {
+    type: 'object',
     value: 12
 };
 
-
+// Update the value property to 89
 myObject.value = 89;
 
-// Use console.log(...) to print the updated value
-console.log(myObject.value);
+// Use console.log(...) to print the updated object
+console.log(myObject);
