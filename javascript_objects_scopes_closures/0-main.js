0766b6740e46d2bf15a3772929dc3cf9dc2aa617
Emmayordi <yordanosteklay424@gmail.com>
2024-02-01 10:47:27 +0100

    Rectangle #1

---
+++ javascript_objects_scopes_closures/0-main.js
@@ -0,0 +1,7 @@
+// 0-main.js
+
+const Rectangle = require('./0-rectangle');
+
+const r1 = new Rectangle();
+console.log(r1);               // Logs: Rectangle {}
+console.log(r1.constructor);   // Logs: [Class: Rectangle]
