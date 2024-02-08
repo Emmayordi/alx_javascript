a7e14674c39138f936b6d42a27c23e497a3892ea
Emmayordi <yordanosteklay424@gmail.com>
2024-02-09 00:10:09 +0100

    more

---
+++ javascript-network/4-completed_tasks.js
@@ -0,0 +1,24 @@
+// 4-completed_tasks.js
+const request = require('request');
+
+// Get the API URL from the command line arguments
+const apiUrl = process.argv[2];
+
+// Make a GET request to the specified API URL
+request.get(apiUrl, (error, response, body) => {
+    if (error) {
+        console.error(`Error: ${error.message}`);
+    } else {
+        try {
+            const tasksData = JSON.parse(body);
+
+            // Create an object to store the number of completed tasks by user ID
+            const completedTasksByUser = {};
+
+            // Iterate through tasks and count completed tasks by user ID
+            tasksData.forEach((task) => {
+                if (task.completed) {
+                    if (completedTasksByUser[task.userId]) {
+                        completedTasksByUser[task.userId]++;
+                    } else {
+                        completedTasksByUser[task.
